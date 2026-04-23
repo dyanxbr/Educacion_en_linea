4c26e27715c0129a7f64fd78f42b962030b6ac8b
Angel <angelroblesbarrientos06@gmail.com>
2026-04-22 23:16:43 -0600

    variable mas escrita y eliminada en la base de datos

--- backend/index.js
+++ backend/index.js
@@ -40,5 +40,5 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
