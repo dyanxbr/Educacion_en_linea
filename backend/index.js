78e7aeea0a60444db04f495a31e5fde6f39f2d95
Angel <angelroblesbarrientos06@gmail.com>
2026-04-22 19:50:58 -0600

    correcion en la api

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,6 @@ app.use((req, res) => {
 
 //  Arrancar servidor   
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
